0394eb7e065738b954a38b29f4664d2817d7d3cd
Alejandro Leone <dantofema@gmail.com>
2024-02-04 23:34:42 -0300

    add favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ $title ?? 'AFIP SOAP EXPLORER' }}</title>
         @vite('resources/css/app.css')
+        <link rel="icon" type="image/x-icon" href="{{ asset('alert.png') }}">
     </head>
     <body>
         {{ $slot }}
